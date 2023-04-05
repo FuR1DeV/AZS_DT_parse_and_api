88e1c2ecaa4536eb27b1b231c67d3471b40df148
Vasia <vasia19rus@bk.ru>
2023-04-05 18:04:22 +0700

    null true in dt fields

--- dt/models.py
+++ dt/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Azs(models.Model):
     latitude = models.FloatField(null=False)
     longitude = models.FloatField(null=False)
-    dt = models.CharField(max_length=150)
-    dt_taneko = models.CharField(max_length=150)
-    dt_winter = models.CharField(max_length=150)
-    dt_arctic = models.CharField(max_length=150)
+    dt = models.CharField(max_length=150, null=True)
+    dt_taneko = models.CharField(max_length=150, null=True)
+    dt_winter = models.CharField(max_length=150, null=True)
+    dt_arctic = models.CharField(max_length=150, null=True)
